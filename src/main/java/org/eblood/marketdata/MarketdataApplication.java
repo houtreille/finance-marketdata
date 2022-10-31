a8fc836bcd2264c1bf9bfe7eb61c79efc48f9702
mhoutreille <mhoutreille@pictet-technologies.com>
2022-10-31 14:23:36 +0100

    first commit

--- src/main/java/org/eblood/marketdata/MarketdataApplication.java
+++ src/main/java/org/eblood/marketdata/MarketdataApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarketdataApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MarketdataApplication.class, args);
+		SpringApplication.run(MarketdataApplication.class, args );
 	}
 
 }
