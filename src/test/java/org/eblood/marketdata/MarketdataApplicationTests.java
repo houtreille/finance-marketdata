ab554a49c9f8d941d76e0d6261bb8a039aac2a88
mhoutreille <mhoutreille@pictet-technologies.com>
2022-10-31 14:18:57 +0100

    first commit

---
+++ src/test/java/org/eblood/marketdata/MarketdataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eblood.marketdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
