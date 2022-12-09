8be7ed6336b1683888fe03d61c4e7bf70622800a
mhoutreille <mhoutreille@pictet-technologies.com>
2022-12-09 16:48:45 +0100

    Create the front end

--- src/main/java/org/eblood/marketdata/controller/InterestRateController.java
+++ src/main/java/org/eblood/marketdata/controller/InterestRateController.java
@@ -4,26 +4,29 @@ import org.eblood.marketdata.model.referenceData.InterestRate;
 import org.eblood.marketdata.model.referenceData.MarketRate;
 import org.eblood.marketdata.service.InterestRateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:64188")
 public class InterestRateController {
 
     @Autowired
     private InterestRateService service;
 
-    @GetMapping("/interestRate/list")
-    public @ResponseBody List<MarketRate> getAllInterestRate(){
+    @GetMapping("/interestRate")
+    public List<MarketRate> getAllInterestRate(){
         return service.getAllInterestRates();
     }
 
-    @PostMapping("/interestRate/create")
-    public void create(MarketRate marketRate){
+    @PostMapping("/createInterestRate")
+    public void create(@RequestBody MarketRate marketRate){
         service.save(marketRate);
     }
 
